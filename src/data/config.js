bc39475575800ccbfa9df8ff6efb004554504a28
saeed koja <saeedkojastudy@gmail.com>
2023-08-12 01:13:15 +0300

    change the port

--- src/data/config.js
+++ src/data/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://192.168.1.111:8000/api";
+const BASE_URL = "http://154.62.109.112:8000/api";
 const AUTH_URL = `${BASE_URL}/auth`;
 const PROFILE_URL = `${BASE_URL}/profile`;
 const CONSULTATIONS_URL = `${BASE_URL}/consultation`;
